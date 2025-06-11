ad216afbe71da6f306e067f6ff7ef8ed95ce2ed2
Fikretand <fikret.andelija1994@gmail.com>
2025-06-11 18:36:26 +0200

    Clean up backend duplicates

---
+++ backend/prisma/migrations/20250611162210_add_phone/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `phone` to the `ContactMessage` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ContactMessage" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_ContactMessage" ("createdAt", "email", "id", "message", "name") SELECT "createdAt", "email", "id", "message", "name" FROM "ContactMessage";
+DROP TABLE "ContactMessage";
+ALTER TABLE "new_ContactMessage" RENAME TO "ContactMessage";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
